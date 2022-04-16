c2a3d30bacfc0cf8337ae8c9003541b051a17135
Даниил <64002850+Odessey18@users.noreply.github.com>
2022-04-16 17:45:03 +0300

    Update Form1.cs
    размер с файлов с папками

--- SimpleFolderSize/Form1.cs
+++ SimpleFolderSize/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SimpleFolderSize
 {
@@ -22,10 +23,21 @@ namespace SimpleFolderSize
             DialogResult result = folderBrowserDialog1.ShowDialog();
             if(result == DialogResult.OK)
             {
-
+                long size = GetDirectorySize(new DirectoryInfo(folderBrowserDialog1.SelectedPath));
+                MessageBox.Show(size.ToString());
             }
         }
 
-
+        private long GetDirectorySize(DirectoryInfo directoryInfo)
+        {
+            long size = 0;
+            FileInfo[] allFiles = directoryInfo.GetFiles();
+            for(int i = 0; i < allFiles.Length;i++)
+            {
+                size += allFiles[i].Length;
+                    
+            }
+            return size;
+        }
     }
 }
